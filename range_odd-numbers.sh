433678c80b75abace298a58ca26ababc64e7823e
Hrishi Ghosalkar <87890571+rookie-cookie-8@users.noreply.github.com>
2023-10-10 19:57:50 +0530

    Create range_odd-numbers.sh

---
+++ range_odd-numbers.sh
@@ -0,0 +1,38 @@
+#Diplsay all the odd numbers within the user specified range
+#!/bin/bash
+
+echo "Enter the first number"
+read first
+echo "Enter the second number"
+read second
+
+if [ -z "$first" ] || [ -z "$second" ]
+then
+        if [ -n "$first" ] && [ -z "$second" ]
+        then
+                echo "Second number field empty"
+        elif [ -z "$first" ] && [ -n "$second" ]
+        then
+                echo "First number field empty"
+        else
+                echo "Something wrong with user input"
+        fi
+
+elif [ -n "$first" ] && [ -n "$second" ]
+then
+        echo "First number is: $first"
+        echo "Second number is: $second"
+        echo "**********************************"
+        echo "Diplay Odd numbers between $first and $second"
+        echo "**********************************"
+        for even in $(seq $first $second)
+        do
+                if(($even%2!=0))
+                then
+                        echo "Odd number: $even"
+                else
+                        continue
+                fi
+
+        done
+fi
