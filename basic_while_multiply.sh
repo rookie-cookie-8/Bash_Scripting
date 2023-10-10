149d7a093b8ce5417caa7bb810ced8a816da9da1
Hrishi Ghosalkar <87890571+rookie-cookie-8@users.noreply.github.com>
2023-10-10 18:21:58 +0530

    Create basic_while_multiply.sh

---
+++ basic_while_multiply.sh
@@ -0,0 +1,39 @@
+#basic multiply while loop
+#check if the multiplication of first and second number 
+#if the result is greater than 1000 then quit, else continue  
+
+#!/bin/bash
+
+echo "Enter the first number"
+read first
+
+echo "Enter the second number"
+read second
+
+if [ -z "$first" ] || [ -z "$second" ]
+then
+        if [ -n "$first" ] && [ -z "$second" ]
+        then
+                echo "Second number field is empty"
+        elif [ -z "$first" ] && [ -n "$second" ]
+        then
+                echo "First number field is empty"
+        else
+                echo "Both number field empty"
+        fi
+elif [ -n "$first" ] && [ -n "$second" ]
+then
+        ans=$((first*second))
+        echo "$first times $second is: $ans"
+        while [ $ans -le 1000 ]
+        do
+                echo "*********************************"
+                ans=$((first*second))
+                echo "$first times $second is: $ans"
+                ((second++))
+                ans=$((first*second))
+
+        done
+echo "Answer is greater than 1000, QUIT"
+
+fi
