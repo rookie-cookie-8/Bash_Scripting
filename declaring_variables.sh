40c54012f2b6da7560329002797c4996823fb4e9
Hrishi Ghosalkar <87890571+rookie-cookie-8@users.noreply.github.com>
2023-10-10 12:33:52 +0530

    Create declaring_variables.sh

---
+++ declaring_variables.sh
@@ -0,0 +1,27 @@
+#This script is to understand how variables can be decalared
+#SYSTEM Defined Variables: $PWD, $HOME, $BASH, $BASH_VERSION, $0, $SHELL, 
+#$HOSTNAME, $HOSTTYPE, $PATH, $LANG, $USER, $LOGNAME, $UID, $RANDOM,
+# $HOSTSIZE, HISTFILESIZE, $HISTFILE, HISTIGNORE, $HISTCONTROL, $HISTTIMEFORMAT
+
+#!/bin/bash
+
+
+a-10
+echo $a
+echo "*******************************************************"
+b="hrishi"
+echo $b
+echo "*******************************************************"
+c=$(date)
+echo $c
+echo "*******************************************************"
+d=$(date | cut -d " " -f1-3)
+echo $d
+echo "*******************************************************"
+e=$(cat /etc/passwd | head -n 3)
+echo $e
+echo "*******************************************************"
+echo "Current working directory: $PWD"
+echo "*******************************************************"
+echo "*******************************************************"
+echo "Current login user: $(whoami)"
