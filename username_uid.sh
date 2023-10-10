3bd8a6fb09c15ea8137c4c9411e84936ffcc3fa5
Hrishi Ghosalkar <87890571+rookie-cookie-8@users.noreply.github.com>
2023-10-10 20:54:22 +0530

    Create username_uid.sh

---
+++ username_uid.sh
@@ -0,0 +1,49 @@
+#find username and uid is available or not for use. 
+
+#!/bin/bash
+
+echo "Enter the username"
+read username
+
+echo "Enter uid"
+read uid
+
+if [ -z "$username" ] || [ -z "$uid" ]
+then
+        if [ -n "$username" ] && [ -z "$uid" ]
+        then
+                echo "UID field empty"
+        elif [ -z "$username" ] && [ -n "$uid" ]
+        then
+                echo "Username field empty"
+        else
+                echo "Soemthing wrong with user input"
+        fi
+
+elif [ -n "$username" ] && [ -n "$uid" ]
+then
+        cat /etc/passwd | cut -d ":" -f1 | grep -i "$username" > ./username-list
+        cat /etc/passwd | cut -d ":" -f3 | grep -i "$uid" > ./uid-list
+        if [ -f ./username-list ] && [ -f ./uid-list ]
+        then
+                if [ -s ./username-list ] && [ -s ./uid-list ]
+                then
+                        echo "Username not available"
+                        echo "uid not available"
+                elif [ ! -s ./username-list ] && [ -s ./uid-list ]
+                then
+                        echo "Username available"
+                        echo "uid not avilable"
+                elif [ ! -s ./uid-list ] && [ -s ./username-list ]
+                then
+                        echo "uid available"
+                        echo "username not avilable"
+                else
+                        echo "Username available"
+                        echo "uid available"
+                fi
+
+        fi
+fi
+rm -rf ./username-list
+rm -rf ./uid-list
