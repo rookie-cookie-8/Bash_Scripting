24f94b7553b89087d1a5d779647b117018359c6d
Hrishi Ghosalkar <87890571+rookie-cookie-8@users.noreply.github.com>
2023-11-27 12:09:42 +0530

    Update basic_while_multiply.sh

--- basic_while_multiply.sh
+++ basic_while_multiply.sh
@@ -32,8 +32,43 @@ then
                 echo "$first times $second is: $ans"
                 ((second++))
                 ans=$((first*second))
-
         done
 echo "Answer is greater than 1000, QUIT"
-
 fi
+---------------------------------------------------------------------------------------------------------------------------------------------------------
+#!/bin/bash
+
+echo "Enter the first number"
+read first
+
+echo "Enter the second"
+read second
+
+if [ -z "$first" ] || [ -z "$second" ]
+then
+        if [ -n "$first" ] && [ -z "$second" ]
+        then
+                echo "Second number field is empty"
+        elif [ -z "$first" ] && [ -n "$second" ]
+        then
+                echo "First number field is empty"
+        else
+                echo "Soemthing wrong with user input"
+        fi
+
+elif [ -n "$first" ] && [ -n "$second" ]
+then
+        ans=$((first*second))
+        while [ $ans -lt 100 ]
+        do
+                ans=$((first*second))
+                echo "*************************************"
+                echo "First number is: $first"
+                echo "Second number is: $second"
+                echo "$first times $second is: $ans"
+                ((second++))
+                ans=$((first*second))
+        done
+fi
+
+
