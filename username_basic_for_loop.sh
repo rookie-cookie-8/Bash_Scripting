118f046f8c071110c5981c0f6b9b02343f2c6cfa
Hrishi Ghosalkar <87890571+rookie-cookie-8@users.noreply.github.com>
2023-11-28 14:35:25 +0530

    Create username_basic_for_loop.sh

---
+++ username_basic_for_loop.sh
@@ -0,0 +1,35 @@
+#Display the username from the specified file as per the user input and used the for loop. For basic understanding
+
+#!/bin/bash
+
+echo "Enter the file location"
+read file_location
+
+if [ -z "$file_location" ]
+then
+        echo "User input missing"
+else
+        if [ -e $file_location ]
+        then
+                if [ -f $file_location ]
+                then
+                        echo "**********************************************"
+                        echo "Lets check the username with for loop"
+                        cat $file_location | cut -d ":" -f1 | sort > ./username_list
+                        sleep 3
+                        for names in $(cat ./username_list )
+                        do
+                                echo "Hello $names"
+                                sleep 0.1
+
+                        done
+                else
+                        echo "File found, but not a regular file"
+                fi
+        else
+                echo "File not found"
+        fi
+
+fi
+rm -rf ./1
+rm -rf ./username_list
