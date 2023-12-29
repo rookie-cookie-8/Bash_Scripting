a2ec1d64b686cda119078715adbdacbbe91661f3
Hrishi Ghosalkar <87890571+rookie-cookie-8@users.noreply.github.com>
2023-12-29 13:46:46 +0530

    Update oscp_js.sh

--- oscp_js.sh
+++ oscp_js.sh
@@ -1,30 +1,63 @@
-#this script for written for oscp exercise, looking for javascript files
+#script created for oscp exercise to look / find javascript files 
 #!/bin/bash
 
-echo "Enter the file location"
-read file_location
+#user input, Enter the absolute path
+echo "Enter the file path"
+read path
 
-if [ -z "$file_location" ]
+#user input, enter the filename
+echo "Enter the file name"
+read filename
+
+#condition check of the user input.
+if [ -z "$path" ] || [ -z "$filename" ]
 then
-        echo "User input missing"
-else
-        if [ -f $file_location ]
+        if [ -z "$path" ] && [ -n "$filename" ]
         then
-                echo "***************************************************"
-                echo "Looking for Javascript files: .js"
-                echo "***************************************************"
-                cat $file_location | cut -d "/" -f5 | grep -i ".js" | sort | uniq | cut -d " " -f1 > ./javascript_files
-                if [ -s ./javascript_files ]
+                echo "Path field empty"
+        elif [ -n "$path" ] && [ -z "$filename" ]
+        then
+                echo "Filename field empty"
+        else
+                echo "User input field missing"
+        fi
+#user input OK. 
+elif [ -n "$path" ] && [ -n "$filename" ]
+then
+        # If the file exists, and the file size is greater than 0 bytes
+        if [ -s $path$filenme ]
+        then
+                echo "***************************************"
+                echo "Looking for Javascript files in $path$filename "
+
+                #file found and looking for javascript files. If found, the output will be saved inside a file i.e. javascript-files
+                cat $path$filename | grep -i ".js" | cut -d "/" -f5 | cut -d " " -f1 | sort | uniq > /home/kali/bash-scripts/javascript_files
+                
+                # If the file exists, and the file size is greater than 0 bytes
+                if [ -s /home/kali/bash-scripts/javascript_files ]
                 then
-                        echo "Javascript files found"
-                        cat ./javascript_files
-                else
-                        echo "No javascript files found"
+                        #user input, ask whether the user would like to view the file contents or not. 
+                        echo "Would you like to view the file contents. Y/y or N/n"
+                        read choice
+
+                        #If user would like to view the file contents, than the files which are found will be displayed
+                        if [ "$choice" == "Y" ] || [ "$choice" == "y" ]
+                        then
+                                cat /home/kali/bash-scripts/javascript_files
+                        else
+
+                                #User would not like to view the file contents, so exit the script. 
+                                echo "Bye for now"
+                        fi
                 fi
         else
-                echo "File not found"
+
+                #User input, If user entered invalid file path or does not exist
+                echo "File doesnt exist or invalid path"
         fi
 
 fi
-rm -rf ./1
-rm -rf ./javascript_files
+
+#Delete the file which were created within the script
+rm -rf /home/kali/bash-scripts/javascript_files
+rm -rf /home/kali/bash-scripts/1
