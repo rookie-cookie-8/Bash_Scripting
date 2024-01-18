1bc1004bedad25eaa652a5d5b6d53c9a33d5fad7
Hrishi Ghosalkar <87890571+rookie-cookie-8@users.noreply.github.com>
2024-01-18 15:41:57 +0530

    Update calculation.sh

--- calculation.sh
+++ calculation.sh
@@ -1,68 +1,61 @@
-#Script for basic calucation
+#basic calculation
+
 #!/bin/bash
 
 echo "Enter the first number"
 read first
-
 echo "Enter the second number"
-read second
+read second 
 
 if [ -z "$first" ] || [ -z "$second" ]
 then
-        if [ -n "$first" ] && [ -z "$second" ]
-        then
-                echo "Second number field is empty"
-        elif [ -z "$first" ] && [ -n "$second" ]
+        if [ -z "$first" ] && [ -n "$second" ]
         then
                 echo "First number field is empty"
+        elif [ -n "$first" ] && [ -z "$second" ]
+        then
+                echo "Second number field is empty"
         else
-                echo "Something wrong with user input"
+                echo "Both number field is empty"
         fi
 
 elif [ -n "$first" ] && [ -n "$second" ]
 then
-        echo "****************************************************"
-        echo "Press 1 for Addition"
-        echo "Press 2 for Subtraction"
-        echo "Press 3 for Multiplication"
-        echo "Press 4 for Quotient"
-        echo "Press 5 for Remainder"
-        echo "Press 6 for Power"
-        echo "****************************************************"
-        read input
-        if [ "$input" == "1" ]
+        echo "**************************************"
+        echo "First number is: $first"
+        echo "Second number is: $second"
+        echo "**************************************"
+        echo "Press 1 --> Addition"
+        echo "Press 2 --> Subtraction"
+        echo "Press 3 --> Multiplication"
+        echo "Press 5 --> Power"
+        echo "Press 5 --> Remainder"
+        echo "Press 6 --> Quotient"
+        read option
+        echo "***********************************************"
+        if [ "$option" == "1" ]
         then
                 ans=$((first+second))
-                echo "$first plus $second is: $ans"
-        elif [ "$input" == "2" ]
+                echo "$first + $second --> $ans"
+        elif [ "$option" == "2" ]
         then
                 ans=$((first-second))
-                echo "$first Subtraction $second is: $ans"
-        elif [ "$input" == "3" ]
+                echo "$first - $second --> $ans"
+        elif [ "$option" == "3" ]
         then
                 ans=$((first*second))
-                echo "$first Multiplication $second is: $ans"
-        elif [ "$input" == "4" ]
-        then
-                if [ $first -ge $second ]
-                then
-                        ans=$((first/second))
-                        echo "$first divide $second is : $ans --> Divisor"
-                else
-                        echo "Second number cannot be greater than first"
-                fi
-        elif [ "$input" == "5" ]
-        then
-                if [ $first -ge $second ]
-                then
-                        ans=$((first%second))
-                        echo "$first divide $second is : $ans --> Remainder"
-                else
-                        echo "Second number cannot be greater than first"
-                fi
-        elif [ "$input" == "6" ]
+                echo "$first * $second --> $ans"
+        elif [ "$option" == "4" ]
         then
                 ans=$((first**second))
-                echo "$first power $second is: $ans"
+                echo "$first power $second --> $ans"
+        elif [ "$option" == "5" ]
+        then
+                ans=$((first%second))
+                echo "$first % $second --> $ans"
+        elif [ "$option" == "6" ]
+        then
+                ans=$((first/second))
+                echo "$first / $second --> $ans"
         fi
 fi
