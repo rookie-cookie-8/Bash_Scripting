2cef07d5add80ec0169407c3c06e15797db066a7
Hrishi Ghosalkar <87890571+rookie-cookie-8@users.noreply.github.com>
2024-02-28 11:15:57 +0530

    Update brute_force_sub-domains.sh

--- brute_force_sub-domains.sh
+++ brute_force_sub-domains.sh
@@ -32,3 +32,41 @@ then
         fi
 
 fi
+---------------------------------------------------------------------------------------------------------------------------------
+#!/bin/bash
+
+echo "Enter the domain name i.e. google.com"
+read domain 
+echo "Enter the sub-domains file path"
+read file_path
+
+if [ -z "$domain" ] || [ -z "$file_path" ]
+then
+        if [ -n "$domain" ] && [ -z "$file_path" ]
+        then
+                echo "File path field is empty"
+        elif [ -z "$domain" ] && [ -n "$file_path" ]
+        then
+                echo "Domain field is empty"
+        else
+                echo "Issues with the user input"
+        fi
+elif [ -n "$domain" ] && [ -n "$file_path" ]
+then
+        if [ -f "$file_path" ] && [ -s "$file_path" ]
+        then
+                test=1
+                for sub_domains in $(cat $file_path)
+                do
+                        echo "************************************"
+                        echo "Testing $test"
+                        host $sub_domains.$domain | grep -i "has address" | cut -d " " -f1
+                        ((test++))
+
+                done
+        else
+                echo "File not found"
+        fi
+fi
+---------------------------------------------------------------------------------------------------------------------------------
+
