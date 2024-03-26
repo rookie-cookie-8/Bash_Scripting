16ead444b394c77a2d3bf64d1eeb30fa09cdaf25
Hrishi Ghosalkar <87890571+rookie-cookie-8@users.noreply.github.com>
2024-03-26 16:44:53 +0530

    Update even_odd_number.sh

--- even_odd_number.sh
+++ even_odd_number.sh
@@ -1,11 +1,11 @@
 #basic script, ask user for numbers and are used in sequence. 
 #Based upon the user input, it display even or odd numbers within the specified range
+#first number must be smaller than second, else error is display if the first number is greater than second
 
 !/bin/bash
 
 echo "Enter the first number"
 read first
-
 echo "Enter the second number"
 read second 
 
@@ -14,43 +14,53 @@ then
         if [ -n "$first" ] && [ -z "$second" ]
         then
                 echo "Second number field is empty"
-        elif [ -n "$second" ] && [ -z "$first" ]
+        elif [ -z "$first" ] && [ -n "$second" ]
         then
                 echo "First number field is empty"
         else
                 echo "Issues with the user input"
         fi
-
 elif [ -n "$first" ] && [ -n "$second" ]
 then
-        echo "************************************************"
-        echo "Would you like to print even or odd number: E/e or O/o"
+        echo "********************************************"
+        echo "Would you like to print even or odd number"
         read option
-        echo "************************************************"
-        if [ "$option" == "e" ] || [ "$option" == "E" ]
+
+        if [ "$option" == "even" ] 
         then
-                for even_number in $(seq $first $second)
-                do
-                        if (($even_number%2==0))
-                        then
-                                echo "Even number is: $even_number"
-                        else
-                                continue
-                        fi
-                done
-        elif [ "$option" == "o" ] || [ "$option" == "O" ]
+                if [ $first -lt $second ]
+                then
+                        for even_numbers in $(seq $first $second)
+                        do
+                                if (($even_numbers%2==0))
+                                then
+                                        echo "Number is --> $even_numbers"
+                                else
+                                        continue
+                                fi
+                        done
+                else
+                        echo "First number cannot be grater than second"
+                fi
+        elif [ "$option" == "odd" ] 
         then
-                for odd_number in $(seq $first $second)
-                do
-                        if (($odd_number%2!=0))
-                        then
-                                echo "Odd number is: $odd_number"
-                        else
-                                continue
-                        fi
-                done
+                if [ $first -lt $second ]
+                then
+                        for odd_numbers in $(seq $first $second)
+                        do
+                                if (($odd_numbers%2!=0))
+                                then
+                                        echo "Number is --> $odd_numbers"
+                                else
+                                        continue
+                                fi
+
+                        done
+                else
+                        echo "First number cannot be greater than second"
+                fi
         else
-                echo "Invalid option, bye"
+                echo "Invalid option"
         fi
 
 fi
