39ff74a186df6aa110fa2fac06c2b557784447d4
Hrishi Ghosalkar <87890571+rookie-cookie-8@users.noreply.github.com>
2024-04-02 11:07:16 +0530

    Update check_username_password_functions.sh

--- check_username_password_functions.sh
+++ check_username_password_functions.sh
@@ -5,44 +5,43 @@ function user_input {
         read username
         echo "Enter the password"
         read password
-        echo "*******************************************"
 }
-
 function validation_1 {
         if [ -z "$username" ] || [ -z "$password" ]
         then
                 if [ -n "$username" ] && [ -z "$password" ]
                 then
-                        echo "Password field is empty"
-                        again
+                        echo "password field is empty"
+                        try_again
                 elif [ -z "$username" ] && [ -n "$password" ]
                 then
-                        echo "Username field is empty"
-                        again
+                        echo "username field is empty"
+                        try_again
                 else
-                        echo "Both the user input field is empty"
-                        again
-                fi
-        fi
-        }
-
-function validation_2 {
-        if [ -n "$username" ] && [ -n "$password" ]
-        then
-                if [ "$username" == "admin" ] && [ "$password" == "password" ]
-                then
-                        echo "Login success"
-                else
-                        echo "Invalid credentials"
-                        echo "*******************************************"
-                        again
-
+                        echo "Issues with the user input"
+                        try_again
                 fi
         fi
 }
-function again {
-        echo "***********************************************************************"
-        echo "Invalid credentials or user input missing, would like like to go again. Y/y or N/n"
+function validation_2 {
+        if [ -n "$username" ] && [ -n "$password" ]
+        then
+                checking
+        fi
+}
+function checking {
+        if [ "$username" == "admin" ] && [ "$password" == "password" ]
+        then
+                echo "*******************************"
+                echo "Login success"
+        else
+                echo "*******************************"
+                echo "Incorrect login credentials"
+                try_again
+        fi
+}
+function try_again {
+        echo "Would you like to try again Y/y or N/n"
         read option
         if [ "$option" == "Y" ] || [ "$option" == "y" ]
         then
@@ -51,13 +50,94 @@ function again {
                 validation_2
         elif [ "$option" == "N" ] || [ "$option" == "n" ]
         then
-                echo "Press NO, so bye for now"
+                echo "Bye for now"
         else
-                echo "Invalid response from the user, so exiting"
+                echo "Invalid user input option"
+                echo "Quiting, bye"
         fi
 }
-
-
 user_input
 validation_1
 validation_2
+------------------------------------------------------------------------------------------------------------------------------------------
+#does require some improvement
+#!/bin/bash
+
+function user_input {
+        echo "Enter the username"
+        read username
+        echo "Enter the password"
+        read password
+}
+function validation_1 {
+        if [ -z "$username" ] || [ -z "$password" ]
+        then
+                if [ -n "$username" ] && [ -z "$password" ]
+                then
+                        echo "password field is empty"
+                        try_again
+                        do_it_again
+                elif [ -z "$username" ] && [ -n "$password" ]
+                then
+                        echo "username field is empty"
+                        try_again
+                        do_it_again
+                else
+                        echo "Issues with the user input"
+                        try_again
+                        do_it_again
+                fi
+        fi
+}
+function validation_2 {
+        if [ -n "$username" ] && [ -n "$password" ]
+        then
+                checking
+        fi
+}
+function checking {
+        if [ "$username" == "admin" ] && [ "$password" == "password" ]
+        then
+                echo "*******************************"
+                echo "Login success"
+                exit
+        else
+                echo "*******************************"
+                echo "Incorrect login credentials"
+                try_again
+                do_it_again
+        fi
+}
+function try_again {
+        echo "Would you like to try again Y/y or N/n"
+        read option
+        if [ "$option" == "Y" ] || [ "$option" == "y" ]
+        then
+                user_input
+                validation_1
+                validation_2
+                do_it_again
+        elif [ "$option" == "N" ] || [ "$option" == "n" ]
+        then
+                echo "Bye for now"
+                exit
+        else
+                echo "Invalid user input option"
+                echo "Quiting, bye"
+                exit
+        fi
+}
+function do_it_again {
+        count=0 
+        while [ $count -lt 3 ]
+        do
+                user_input
+                validation_1
+                validation_2
+                ((count++))
+        done
+}
+user_input
+validation_1
+validation_2
+
