bf95b417673d39d3fbda36a9edcb7e473e6c6319
Hrishi Ghosalkar <87890571+rookie-cookie-8@users.noreply.github.com>
2024-04-02 11:46:47 +0530

    Update range_even-numbers.sh

--- range_even-numbers.sh
+++ range_even-numbers.sh
@@ -74,4 +74,44 @@ then
         done
 
 fi
+------------------------------------------------------------------------------------------------------------------------
+#!/bin/bash
 
+echo "Enter the first number"
+read first
+echo "Enter the second number"
+read second
+
+if [ -z "$first" ] || [ -z "$second" ] 
+then
+        if [ -n "$first" ] && [ -z "$second" ]
+        then
+                echo "Second number field is empty"
+        elif [ -z "$first" ] && [ -n "$second" ]
+        then
+                echo "First number field is empty"
+        else
+                echo "Issues with the user input"
+        fi
+
+
+elif [ -n "$first" ] && [ -n "$second" ]
+then
+        if [ $first -lt $second ]
+        then
+                        echo "*****Printing even numbers in range between $first and $second***************"
+                for even_numbers in $(seq $first $second)
+                do
+                        if (($even_numbers%2==0))
+                        then
+                                echo "NUmber --> $even_numbers"
+                        else
+                                continue
+                        fi
+
+                done
+        else
+                echo "First number cannot be grater than second" 
+        fi
+
+fi
