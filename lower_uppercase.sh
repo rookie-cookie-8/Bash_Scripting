d0db113f8f0637c544a83640ff37914b6c4badb9
Hrishi Ghosalkar <87890571+rookie-cookie-8@users.noreply.github.com>
2024-04-03 13:44:58 +0530

    Update lower_uppercase.sh

--- lower_uppercase.sh
+++ lower_uppercase.sh
@@ -23,4 +23,25 @@ then
 else
         echo "Name is on lower case"
 fi
+-------------------------------------------------------------------------------------------------------------------------
+#!/bin/bash
 
+echo "Enter your full name"
+read full_name
+
+if [ -z "$full_name" ]
+then
+        echo "User input empty"
+else
+        if [ ${#full_name} -lt 5 ]
+        then
+                echo "Full name must be greater than 5"
+        else
+                if [ "$full_name" == "${full_name^^}" ]
+                then
+                        echo "Name is -----> $full_name"
+                else
+                        echo "Name is --> ${full_name^^}"
+                fi
+        fi
+fi
