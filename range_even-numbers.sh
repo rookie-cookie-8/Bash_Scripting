6fee061305d1d009b1aaa3b3864d8239c9acaa44
Hrishi Ghosalkar <87890571+rookie-cookie-8@users.noreply.github.com>
2024-04-26 18:14:54 +0530

    Update range_even-numbers.sh

--- range_even-numbers.sh
+++ range_even-numbers.sh
@@ -7,7 +7,6 @@
 
 echo "Enter the first number"
 read first
-
 echo "Enter the second number"
 read second 
 
@@ -15,60 +14,46 @@ if [ -z "$first" ] || [ -z "$second" ]
 then
         if [ -n "$first" ] && [ -z "$second" ]
         then
+                echo "***************************************"
                 echo "Second number field is empty"
         elif [ -z "$first" ] && [ -n "$second" ]
         then
+                echo "***************************************"
                 echo "First number field is empty"
         else
+                echo "***************************************"
                 echo "Issues with the user input"
         fi
 elif [ -n "$first" ] && [ -n "$second" ]
 then
-        echo "**************************************************"
-        if [ $first -lt $second ] && [ $first -ne $second ]
+        if [ $first -lt $second ]
         then
-                echo "Would you like to print even or odd numbers"
-                read option
-                if [ "$option" == "even" ]
+                echo "******************************************"
+                echo "Would you like to print odd or even number?"
+                read answer
+                if [ "$answer" == "odd" ]
                 then
-                        echo "Displaying even numbers"
-                        sleep 0.5
-                        for even_numbers in $(seq $first $second)
-                        do
-                                if (($even_numbers%2==0))
-                                then
-                                        echo "Even number --> $even_numbers"
-                                else
-                                        continue
-                                fi
-                        done
-                elif [ "$option" == "odd" ]
-                then
-                        echo "Displaying odd numbers"
-                        sleep 0.5
                         for odd_numbers in $(seq $first $second)
                         do
                                 if (($odd_numbers%2!=0))
                                 then
-                                        echo "Odd number --> $odd_numbers"
-                                else
-                                        continue
+                                        echo "Odd number is --> $odd_numbers"
+                                fi
+                        done
+                elif [ "$answer" == "even" ]
+                then
+                        for even_numbers in $(seq $first $second)
+                        do
+                                if (($even_numbers%2==0))
+                                then
+                                        echo "Even number is --> $even_numbers"
                                 fi
                         done
                 else
-                        echo "Issues with the user input"
+                        echo "Invalid user input"
                 fi
-
-        elif [ $first -eq $second ]
-        then
-                echo "First number is equal to second"
-                echo "Cannot do anything"
-        elif [ $first -gt $second ]
-        then
-                echo "Second number is greater than second"
-                echo "Cannot do anything"
         else
-                echo "Something went wrong" 
+                echo "Second number is less than first"
+                echo "Nothing to do"
         fi
-
 fi
