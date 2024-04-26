4ea0e41e83de1226527cc5ea8c48f52033914ff3
Hrishi Ghosalkar <87890571+rookie-cookie-8@users.noreply.github.com>
2024-04-26 17:20:54 +0530

    oscp_js_exercise.sh

--- oscp_5.7.3.1.3.sh
+++ oscp_5.7.3.1.3.sh
@@ -3,73 +3,35 @@
 #!/bin/bash
 
 echo "Enter the file path"
-read path
-echo "Enter the filename"
-read file
-
-if [ -z "$path" ] || [ -z "$file" ]
-then
-        if [ -n "$path" ] && [ -z "$file" ]
-        then
-                echo "File field not found"
-        elif [ -z "$path" ] && [ -n "$file" ]
-        then
-                echo "Path field not found"
-        else
-                echo "Issues with the user input"
-        fi
-elif [ -n "$path" ] && [ -n "$file" ]
-then
-        if [ -e $path$file ]
-        then
-                if [ -f $path$file ]
-                then
-                        cat $path$file | grep -i ".js" | cut -d "/" -f5 | sort | uniq > ./javascript_files
-                        if [ -s ./javascript_files ]
-                        then
-                                echo "*****************************************"
-                                echo "File found"
-                                echo "*****************************************"
-                                cat ./javascript_files
-                                echo "*****************************************"
-                        else
-                                echo "No javascript files"
-                        fi
-                else
-                        echo "No file found"
-                fi
-        else
-                echo "*******************************************"
-                echo "Invalid file path"
-        fi
-fi
-rm -rf ./1
-rm -rf ./javascript_files
------------------------------------------------------------------------------------------------------------------------------------
-#!/bin/bash
-
-echo "Enter the file path"
 read filepath
 
 if [ -z "$filepath" ]
 then
-        echo "File path field is empty"
+        echo "File path field empty"
 else
-        echo "**********************************"
-        if [ -f "$filepath" ] && [ -s "$filepath" ]
+        if [ -e $filepath ]
         then
-                cat $filepath | grep -i "js" | cut -d "/" -f5 | sort | uniq > /home/kali/bash-scripts/javascript_files 
-                if [ -s /home/kali/bash-scripts/javascript_files ]
+                if [ -d $filepath ]
                 then
-                        cat ./javascript_files
+                        echo "Its a directory"
+                elif [ -f $filepath ]
+                then
+                        cat $filepath | grep -i ".js" | cut -d "/" -f5 | sort | uniq > javascript_files
+                        if [ -s ./javascript_files ]
+                        then
+                                echo "****************************"
+                                cat ./javascript_files
+                        else
+                                echo "Unable to find Javascript files"
+                        fi
                 else
-                        echo "No contents found"
+                        echo "Unable to identify file type"
                 fi
         else
-                echo "File not found"
+                echo "Unable to locate file"
         fi
-
 fi
 rm -rf ./1
 rm -rf ./javascript_files
+
 
