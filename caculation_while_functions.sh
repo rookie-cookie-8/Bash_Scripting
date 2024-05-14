059c73c26bf717b15b1ca3e12b70fee8ed8564e0
Hrishi Ghosalkar <87890571+rookie-cookie-8@users.noreply.github.com>
2024-05-14 16:03:03 +0530

    Update caculation_while_functions.sh

--- caculation_while_functions.sh
+++ caculation_while_functions.sh
@@ -3,71 +3,79 @@
 #!/bin/bash
 
 function user_input {
-        echo "ENter the first number"
+        echo "Enter the first number"
         read first
         echo "Enter the second number"
         read second
 }
+
 function validation_1 {
         if [ -z "$first" ] || [ -z "$second" ]
         then
                 if [ -n "$first" ] && [ -z "$second" ]
                 then
+                        echo "**************************************"
                         echo "Second number field is empty"
                         try_again
                 elif [ -z "$first" ] && [ -n "$second" ]
                 then
+                        echo "**************************************"
                         echo "First number field is empty"
                         try_again
                 else
+                        echo "**************************************"
                         echo "Issues with the user input"
                         try_again
                 fi
-
         fi
 }
 function validation_2 {
         if [ -n "$first" ] && [ -n "$second" ]
         then
-                echo "*************************************"
+                echo "**************************************************"
                 echo "Press 1 --> Addition"
                 echo "Press 2 --> Subtraction"
                 echo "Press 3 --> Multiplication"
                 echo "Press 4 --> Power"
-                read option 
-                if [ "$option" == "1" ]
+                read user_option
+                if [ "$user_option" == "1" ]
                 then
+                        echo "******************************************"
                         ans=$((first+second))
-                        echo "*************************************"
                         echo "$first plus $second --> $ans"
+                        echo "******************************************"
                         try_again
-                elif [ "$option" == "2" ]
+                elif [ "$user_option" == "2" ]
                 then
+                        echo "******************************************"
                         ans=$((first-second))
-                        echo "*************************************"
                         echo "$first minus $second --> $ans"
+                        echo "******************************************"
                         try_again
-                elif [ "$option" == "3" ]
+                elif [ "$user_option" == "3" ]
                 then
+                        echo "******************************************"
                         ans=$((first*second))
-                        echo "*************************************"
                         echo "$first multiply $second --> $ans"
+                        echo "******************************************"
                         try_again
-                elif [ "$option" == "4" ]
+                elif [ "$user_option" == "4" ]
                 then
+                        echo "******************************************"
                         ans=$((first**second))
-                        echo "*************************************"
                         echo "$first power $second --> $ans"
+                        echo "******************************************"
                         try_again
                 else
-                        echo "*************************************"
+                        echo "******************************************"
                         echo "Invalid user input"
                         try_again
                 fi
         fi
 }
 function try_again {
-        echo "Would you like to try again: Y/y or N/n"
+        echo "**********************************************"
+        echo "Would you like to try again"
         read answer
         if [ "$answer" == "Y" ] || [ "$answer" == "y" ]
         then
@@ -76,11 +84,20 @@ function try_again {
                 validation_2
         elif [ "$answer" == "N" ] || [ "$answer" == "n" ]
         then
+                echo "**********************************************"
                 echo "Bye"
+                exit
         else
-                echo "Invalid user input"
+                echo "**********************************************"
+                echo "Invalid user option"
+                exit
         fi
 }
+
+user_input
+validation_1
+validation_2
+
 user_input
 validation_1
 validation_2
