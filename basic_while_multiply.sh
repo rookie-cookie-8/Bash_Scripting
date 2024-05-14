ab30031f3c3317acb15afd73a24c4683871b030f
Hrishi Ghosalkar <87890571+rookie-cookie-8@users.noreply.github.com>
2024-05-14 13:20:32 +0530

    Update basic_while_multiply.sh

--- basic_while_multiply.sh
+++ basic_while_multiply.sh
@@ -1,7 +1,9 @@
 #Basic while loop: User inputs the first number, multiplier, and stop number. 
 #The first number is then multiplied by the multiplier until reaching the stop number
+#if the result of multiplication i.e. first times $multiplier is greater than stop_number .. nothing to do
+#or else do the multiplication until the result is less than stop_number
 
-#!/bin/bash
+#!/bin/bash                                                                
 
 echo "Enter the first number"
 read first
@@ -14,40 +16,48 @@ if [ -z "$first" ] || [ -z "$multiplier" ] || [ -z "$stop_number" ]
 then
         if [ -n "$first" ] && [ -z "$multiplier" ] && [ -z "$stop_number" ]
         then
-                echo "**********************************"
-                echo "Multiplier field is empty"
-                echo "Stop number field is empty"
+                echo "*************************************************"
+                echo "Multiplier number field empty"
+                echo "Stop number field empty"
+                echo "*************************************************"
         elif [ -n "$first" ] && [ -n "$multiplier" ] && [ -z "$stop_number" ]
         then
-                echo "**********************************"
-                echo "Stop number field is empty"
+                echo "*************************************************"
+                echo "Stop number field empty"
+                echo "*************************************************"
         elif [ -n "$first" ] && [ -z "$multiplier" ] && [ -n "$stop_number" ]
         then
-                echo "**********************************"
-                echo "Multiplier field is empty"
+                echo "*************************************************"
+                echo "Multiplier number field empty"
+                echo "*************************************************"
         elif [ -z "$first" ] && [ -n "$multiplier" ] && [ -n "$stop_number" ]
         then
-                echo "**********************************"
-                echo "First number field is empty"
-        elif [ -z "$first" ] && [ -n "$multiplier" ] && [ -z "$stop_number" ]
-        then
-                echo "**********************************"
-                echo "First number field is empty"
-                echo "Stop number field is empty"
+                echo "*************************************************"
+                echo "First number field empty"
+                echo "*************************************************"
         elif [ -z "$first" ] && [ -z "$multiplier" ] && [ -n "$stop_number" ]
         then
-                echo "**********************************"
-                echo "First number field is empty"
-                echo "Multiplier field is empty"
+                echo "*************************************************"
+                echo "First number field empty"
+                echo "Multiplier number field empty"
+                echo "*************************************************"
+        elif [ -z "$first" ] && [ -n "$multiplier" ] && [ -z "$stop_number" ]
+        then
+                echo "*************************************************"
+                echo "First number field empty"
+                echo "Stop number field empty"
+                echo "*************************************************"
         elif [ -z "$first" ] && [ -z "$multiplier" ] && [ -z "$stop_number" ]
         then
-                echo "**********************************"
-                echo "First number field is empty"
-                echo "Multiplier field is empty"
-                echo "Stop number field is empty"
+                echo "*************************************************"
+                echo "First number field empty"
+                echo "Multiplier number field empty"
+                echo "Stop number field empty"
+                echo "*************************************************"
         else
-                echo "**********************************"
-                echo "Something went wrong"
+                echo "*************************************************"
+                echo "Issues with the user input"
+                echo "*************************************************"
         fi
 elif [ -n "$first" ] && [ -n "$multiplier" ] && [ -n "$stop_number" ]
 then
@@ -56,15 +66,16 @@ then
         then
                 while [ $ans -lt $stop_number ]
                 do
-                        echo "***********************************"
+                echo "*************************************************"
                         echo "$first times $multiplier --> $ans"
                         ((multiplier++))
                         ans=$((first*multiplier))
                 done
         else
-                echo "***********************************"
+                echo "*************************************************"
                 echo "$first times $multiplier --> $ans"
                 echo "$ans is greater than $stop_number"
                 echo "Cant do anything"
+                echo "*************************************************"
         fi
 fi
