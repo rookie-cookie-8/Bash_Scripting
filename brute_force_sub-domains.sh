b84b33adc08ffb03f3572b969b63fc9711cff7c6
Hrishi Ghosalkar <87890571+rookie-cookie-8@users.noreply.github.com>
2024-05-14 15:25:04 +0530

    Update brute_force_sub-domains.sh

--- brute_force_sub-domains.sh
+++ brute_force_sub-domains.sh
@@ -1,65 +1,52 @@
-#brute force attack on the domain to find sub-domains / different hostnames
-# file: /usr/share/seclists/Discovery/DNS/subdomains-top1million-5000.txt
+#Brute force attack on the domain in order to find out different hostname
 
 #!/bin/bash
 
-echo "Enter the domain name i.e. domain.com"
+echo "Enter the domain name i.e. google.com"
 read domain
-echo "Enter the filepath for brute force"
-read filepath
+echo "Enter the file for the brute force"
+read filename
 
-if [ -z "$domain" ] || [ -z "$filepath" ]
+if [ -z "$domain" ] || [ -z "$filename" ]
 then
-        if [ -n "$domain" ] && [ -z "$filepath" ]
+        if [ -n "$domain" ] && [ -z "$filename" ]
         then
-                echo "Filepath field is empty"
-        elif [ -z "$domain" ] && [ -n "$filepath" ]
+                echo "***************************************"
+                echo "Filename field is empty"
+                echo "***************************************"
+        elif [ -z "$domain" ] && [ -n "$filename" ]
         then
-                echo "Domain field is empty"
+                echo "***************************************"
+                echo "Domain name field is empty"
+                echo "***************************************"
         else
+                echo "***************************************"
                 echo "Issues with the user input"
+                echo "***************************************"
         fi
-elif [ -n "$domain" ] && [ -n "$filepath" ]
+elif [ -n "$domain" ] && [ -n "$filename" ]
 then
-        if [ -e $filepath ]
+        if [ -f $filename ]
         then
-                if [ -f $filepath ]
-                then
-                        echo "********************************************"
-                        echo "Will start brute force attack on $domain"
-                        sleep 2
+                for domain_name in $(cat $filename)
+                do
+                        host $domain_name.$domain 1>/dev/null
+                        if [ $? -eq 0 ]
+                        then
+                                echo "**************************************"
+                                host $domain_name.$domain | cut -d " " -f1,4
+                                sleep 0.2
+                        else
+                                echo "**************************************"
+                                echo "$domain_name.$domain --> Public DNS records not found"
+                                sleep 0.2
+                                continue
+                        fi
 
-                        for host_name in $(cat $filepath)
-                        do
-                                host $host_name.$domain > /dev/null
-                                if [ $? -eq 0 ]
-                                then
-                                        echo "********************************************"
-                                        host $host_name.$domain 
-                                        sleep 0.2
-                                        continue
-                                        echo "********************************************"
-                                elif [ $? -eq 1 ]
-                                then
-                                        echo "********************************************"
-                                        echo "$host_name.$domain invalid"
-                                        sleep 0.2
-                                        continue
-                                        echo "********************************************"
-                                else
-                                        echo "Something went wrong with $filepath.$domain"
-                                fi
-                        done
-
-                elif [ -d $filepath ]
-                then
-                        echo "********************************"
-                        echo "Its a directory, cant do anything"
-                else
-                        echo "Unable to identify the file type"
-                fi
+                done
         else
-                echo "********************************"
+                echo "***************************************"
                 echo "File not found"
+                echo "***************************************"
         fi
 fi
