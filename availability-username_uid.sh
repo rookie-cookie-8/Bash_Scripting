cdbf383323d5532e30b909692d2a883868b80c2a
Hrishi Ghosalkar <87890571+rookie-cookie-8@users.noreply.github.com>
2024-05-14 12:57:25 +0530

    Update availability-username_uid.sh

--- availability-username_uid.sh
+++ availability-username_uid.sh
@@ -1,58 +1,57 @@
-# username and uid number as input from the user.
-#script check whether the username and uid number is available or not. 
+#username and uid_number input from the user
+#script tells whether the username and the uid_number is available for the next use. 
 
 #!/bin/bash
-
 echo "Enter the username"
 read username
 echo "Enter the uid number"
-read uid
+read uid_number
 
-if [ -z "$username" ] || [ -z "$uid" ]
+if [ -z "$username" ] || [ -z "$uid_number" ]
 then
-        if [ -n "$username" ] && [ -z "$uid" ]
+        if [ -n "$username" ] && [ -z "$uid_number" ]
         then
-                echo "UID field is empty"
-        elif [ -z "$username" ] && [ -n "$uid" ]
+                echo "UID number field is empty"
+        elif [ -z "$username" ] && [ -n "$uid_number" ]
         then
                 echo "Username field is empty"
         else
-                echo "Issus with the user input"
+                echo "Issues with the user input"
         fi
-
-elif [ -n "$username" ] && [ -n "$uid" ]
+elif [ -n "$username" ] && [ -n "$uid_number" ]
 then
-        cat /etc/passwd | cut -d ":" -f1 | grep -i "$username" > ./username_list
-        cat /etc/passwd | cut -d ":" -f3 | grep -i "$uid" > ./uid_list
-        if [ ! -s ./username_list ] && [ ! -s ./uid_list ]
+        cat /etc/passwd | grep -i "$username" | cut -d ":" -f1 > ./username_lists
+        cat /etc/passwd | grep -i "$uid_number" | cut -d ":" -f3 > ./uid_lists
+        if [ ! -s ./username_lists ] && [ ! -s ./uid_lists ]
         then
-                echo "***********************************************"
+                echo "********************************************"
                 echo "Username is available --> $username"
-                echo "UID is available --> $uid"
-                echo "***********************************************"
-        elif [ -s ./username_list ] && [ ! -s ./uid_list ]
+                echo "UID is available --> $uid_number"
+                echo "********************************************"
+        elif [ -s ./username_lists ] && [ ! -s ./uid_lists ]
         then
-                echo "***********************************************"
+                echo "********************************************"
                 echo "Username is not available --> $username"
-                echo "UID is available --> $uid"
-                echo "***********************************************"
-        elif [ ! -s ./username_list ] && [ -s ./uid_list ]
+                echo "UID is available --> $uid_number"
+                echo "********************************************"
+        elif [ ! -s ./username_lists ] && [ -s ./uid_lists ]
         then
-                echo "***********************************************"
+                echo "********************************************"
                 echo "Username is available --> $username"
-                echo "UID is not available --> $uid"
-                echo "***********************************************"
-        elif [ -s ./username_list ] && [ -s ./uid_list ]
+                echo "UID is not available --> $uid_number"
+                echo "********************************************"
+        elif [ -s ./username_lists ] && [ -s ./uid_lists ]
         then
-                echo "***********************************************"
+                echo "********************************************"
                 echo "Username is not available --> $username"
-                echo "UID is not available --> $uid"
-                echo "***********************************************"
+                echo "UID is not available --> $uid_number"
+                echo "********************************************"
         else
-                echo "***********************************************"
-                echo "SOmething went wrong"
+                echo "********************************************"
+                echo "Snomething went wrong"
+                echo "********************************************"
         fi
 fi
-rm -rf ./username_list
-rm -rf ./uid_list
 rm -rf ./1
+rm -rf ./username_lists
+rm -rf ./uid_lists
