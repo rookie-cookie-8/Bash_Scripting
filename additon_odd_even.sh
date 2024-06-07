ddcd8851130d6678514313631fd5741028692904
Hrishi Ghosalkar <87890571+rookie-cookie-8@users.noreply.github.com>
2024-06-07 11:57:57 +0530

    Update additon_odd_even.sh

--- additon_odd_even.sh
+++ additon_odd_even.sh
@@ -1,41 +1,50 @@
-#script to add two numbers and check whether the result is odd or even number
+#two numbers as input from users
+#add the two numbers and whether the result of addition is even or odd number 
 
 #!/bin/bash
 
 echo "Enter the first number"
 read first
 echo "Enter the second number"
-read second 
+read second
 
 if [ -z "$first" ] || [ -z "$second" ]
 then
-        if [ -z "$first" ] && [ -n "$second" ]
+        if [ -n "$first" ] && [ -z "$second" ]
         then
+                echo "*********************************"
+                echo "Second number field is empty"
+        elif [ -z "$first" ] && [ -n "$second" ]
+        then
+                echo "*********************************"
                 echo "First number field is empty"
-        elif [ -n "$first" ] && [ -z "$second" ]
+        elif [ -z "$first" ] && [ -z "$second" ]
         then
+                echo "*********************************"
+                echo "First number field is empty"
                 echo "Second number field is empty"
         else
+                echo "*********************************"
                 echo "Issues with the user input"
         fi
 elif [ -n "$first" ] && [ -n "$second" ]
 then
+        echo "*************************************"
+        echo "First number is --> $first"
+        echo "Second number is --> $second"
         ans=$((first+second))
         if (($ans%2==0))
         then
-                echo "*****************************************"
-                echo "$first plus $second --> $ans"
-                echo "$ans --> Even number"
-                echo "*****************************************"
+                echo "*************************************"
+                echo "$first + $second is --> $ans"
+                echo "$ans is a even number"
         elif (($ans%2!=0))
         then
-                echo "*****************************************"
-                echo "$first plus $second --> $ans"
-                echo "$ans --> Odd number"
-                echo "*****************************************"
+                echo "*************************************"
+                echo "$first + $second is --> $ans"
+                echo "$ans is a odd number"
         else
-                echo "*****************************************"
-                echo "Something went wrong"
-                echo "*****************************************"
+                echo "*************************************"
+                echo "Something went wrong with calculation"
         fi
 fi
