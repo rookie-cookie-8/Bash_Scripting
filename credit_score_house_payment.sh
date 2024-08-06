0449c8485cade12ee4638317b67deeb61841698c
Hrishi Ghosalkar <87890571+rookie-cookie-8@users.noreply.github.com>
2024-08-06 13:44:05 +0530

    Update credit_score_house_payment.sh

--- credit_score_house_payment.sh
+++ credit_score_house_payment.sh
@@ -46,3 +46,84 @@ then
 
         fi
 fi
+
+-------------------------------------------------------------------------------------------------------------------------------------------
+#!/bin/bash
+
+echo "Enter the total cost of the house"
+read total
+echo "Enter the credit score"
+read credit
+
+if [ -z "$total" ] || [ -z "$credit" ]
+then
+        if [ -n "$total" ] && [ -z "$credit" ]
+        then
+                echo "********************************"
+                echo "Credit score field is empty"
+        elif [ -z "$total" ] && [ -n "$credit" ]
+        then
+                echo "********************************"
+                echo "Total cost field is empty"
+        elif [ -z "$total" ] && [ -z "$credit" ]
+        then
+                echo "********************************"
+                echo "Total cost field is empty"
+                echo "Credit score field is empty"
+        else
+                echo "********************************"
+                echo "Issues with the user input"
+        fi
+
+elif [ -n "$total" ] && [ -n "$credit" ]
+then
+        if [[ "$total" =~ ^[0-9]+$ ]] && [[ "$credit" =~ ^[0-9]+$ ]]
+        then
+                if [ "$credit" -ge 750 ]
+                then
+                        down_payment=$(($total*10/100))
+                        echo "*************************************"
+                        echo "Total cost of the house --> $total"
+                        echo "Credit score --> $credit"
+                        echo "Down payment --> $down_payment"
+                elif [ "$credit" -lt 750 ] && [ "$credit" -ge 650 ]
+                then
+                        down_payment=$(($total*20/100))
+                        echo "*************************************"
+                        echo "Total cost of the house --> $total"
+                        echo "Credit score --> $credit"
+                        echo "Down payment --> $down_payment"
+                elif [ "$credit" -lt 650 ] && [ "$credit" -ge 600 ]
+                then
+                        down_payment=$(($total*30/100))
+                        echo "*************************************"
+                        echo "Total cost of the house --> $total"
+                        echo "Credit score --> $credit"
+                        echo "Down payment --> $down_payment"
+                else
+                        echo "*************************************"
+                        echo "Credit score is low"
+                        echo "Cannot process the application"
+                fi
+        elif [[ ! "$total" =~ ^[0-9]+$ ]] || [[ ! "$credit" =~ ^[0-9]+$ ]]
+        then
+                if [[ ! "$total" =~ ^[0-9]+$ ]] && [[ "$credit" =~ ^[0-9]+$ ]]
+                then
+                        echo "********************************************"
+                        echo "Total cost of the house field can only contain numeric digits"
+                elif [[ "$total" =~ ^[0-9]+$ ]] && [[ ! "$credit" =~ ^[0-9]+$ ]]
+                then
+                        echo "********************************************"
+                        echo "Credit score field can only contain numeric digits"
+                elif [[ ! "$total" =~ ^[0-9]+$ ]] && [[ ! "$credit" =~ ^[0-9]+$ ]]
+                then
+                        echo "********************************************"
+                        echo "Total cost of the house field can only contain numeric digits"
+                        echo "Credit score field can only contain numeric digits"
+                else
+                        echo "********************************************"
+                        echo "Something went wrong"
+                fi
+        fi
+fi
+
