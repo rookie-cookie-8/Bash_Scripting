716bf662dfa3620229bd100471079f36b3a93be1
Hrishi Ghosalkar <87890571+rookie-cookie-8@users.noreply.github.com>
2024-08-06 12:09:35 +0530

    Update basic_while_multiply.sh

--- basic_while_multiply.sh
+++ basic_while_multiply.sh
@@ -77,3 +77,98 @@ then
                 echo "Something went wrong"
         fi
 fi
+
+-------------------------------------------------------------------------------------------------------------------------------------------------------------
+#advanced
+#!/bin/bash
+
+echo "Enter the first number"
+read first
+echo "Enter the multiplier number"
+read multiplier
+echo "Enter the stop_number"
+read stop_number
+
+if [ -z "$first" ] || [ -z "$multiplier" ] || [ -z "$stop_number" ]
+then
+        if [ -n "$first" ] && [ -z "$multiplier" ] && [ -z "$stop_number" ]
+        then
+                echo "***************************************"
+                echo "Multiplier field is empty"
+                echo "Stop number field is empty"
+        elif [ -n "$first" ] && [ -n "$multiplier" ] && [ -z "$stop_number" ]
+        then
+                echo "***************************************"
+                echo "Stop number field is empty"
+        elif [ -n "$first" ] && [ -z "$multiplier" ] && [ -n "$stop_number" ]
+        then
+                echo "***************************************"
+                echo "Multiplier field is empty"
+        elif [ -z "$first" ] && [ -n "$multiplier" ] && [ -n "$stop_number" ]
+        then
+                echo "***************************************"
+                echo "First number field is empty"
+        elif [ -z "$first" ] && [ -z "$multiplier" ] && [ -n "$stop_number" ]
+        then
+                echo "***************************************"
+                echo "First number field is empty"
+                echo "Multiplier field is empty"
+        elif [ -z "$first" ] && [ -n "$multiplier" ] && [ -z "$stop_number" ]
+        then
+                echo "***************************************"
+                echo "First number field is empty"
+                echo "Stop number field is empty"
+        elif [ -z "$first" ] && [ -z "$multiplier" ] && [ -z "$stop_number" ]
+        then
+                echo "***************************************"
+                echo "First number field is empty"
+                echo "Multiplier field is empty"
+                echo "Stop number field is empty"
+        else
+                echo "***************************************"
+                echo "Issues with the user input"
+        fi
+elif [ -n "$first" ] && [ -n "$multiplier" ] && [ -n "$stop_number" ]
+then
+        if [[ "$first" =~ ^[0-9]+$ ]] && [[ "$multiplier" =~ ^[0-9]+$ ]] && [[ "$stop_number" =~ ^[0-9]+$ ]]
+        then
+                ans=$((first*multiplier))
+                if [ $ans -lt $stop_number ]
+                then
+                        while [ $ans -lt $stop_number ]
+                        do
+                                ans=$((first*multiplier))
+                                echo "$first times $multiplier --> $ans"
+                                ((multiplier++))
+                                ans=$((first*multiplier))
+                                sleep 0.3
+                        done
+                elif [ $ans -ge $stop_number ]
+                then
+                        if [ $ans -eq $stop_number ]
+                        then
+                                echo "**********************************"
+                                echo "$first times $multiplier --> $ans"
+                                echo $ans equal to $stop_number
+                                echo "Cannot do anything"
+                        elif [ $ans -gt $stop_number ]
+                        then
+                                echo "**********************************"
+                                echo "$first times $multiplier --> $ans"
+                                echo "$ans is greater than $stop_number"
+                                echo "Cannot do anything"
+                        else
+                                echo "**********************************"
+                                echo "Something went wrong with caculation"
+                        fi
+
+                fi
+        elif [[ ! "$first" =~ ^[0-9]+$ ]] || [[ ! "$multiplier" =~ ^[0-9]+$ ]] || [[ ! "$stop_number" =~ ^[0-9]+$ ]]
+        then
+                echo "************************************"
+                echo "User input must contain only numeric digits"
+                echo "Kindly check the user input"
+        fi
+fi
+
+
