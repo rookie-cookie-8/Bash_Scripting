de04a28ea6f0658368830f3203129fc4dc047c25
Hrishi Ghosalkar <87890571+rookie-cookie-8@users.noreply.github.com>
2024-08-06 12:51:54 +0530

    Update caculation_while_functions.sh

--- caculation_while_functions.sh
+++ caculation_while_functions.sh
@@ -101,3 +101,96 @@ validation_2
 user_input
 validation_1
 validation_2
+
+--------------------------------------------------------------------------------------------------------------------------------------------
+#!/bin/bash
+
+function user_input {
+        echo "Enter the first number"
+        read first
+        echo "Enter the second number"
+        read second
+}
+function validation_1 {
+        if [ -z "$first" ] || [ -z "$second" ]
+        then
+                if [ -n "$first" ] && [ -z "$second" ]
+                then
+                        echo "*************************************"
+                        echo "Second number field empty"
+                elif [ -z "$first" ] && [ -n "$second" ]
+                then
+                        echo "*************************************"
+                        echo "First number field empty"
+                elif [ -z "$first" ] && [ -z "$second" ]
+                then
+                        echo "*************************************"
+                        echo "First number field empty"
+                        echo "Second number field empty"
+                else
+                        echo "*************************************"
+                        echo "Issues with the user input"
+                fi
+        fi
+}
+function validation_2 {
+        if [ -n "$first" ] && [ -n "$second" ]
+        then
+                if [[ "$first" =~ ^[0-9-]+$ ]] && [[ "$second" =~ ^[0-9-]+$ ]]
+                then
+                        echo "***************************************************"
+                        echo "Press 1 --> Addition"
+                        echo "Press 2 --> Subtraction"
+                        echo "Press 3 --> Multiplication"
+                        echo "Press 4 --> Power"
+                        read option
+                        if [ "$option" == "1" ]
+                        then
+                                echo "**********************************"
+                                ans=$((first+second))
+                                echo "$first plus $second --> $ans"
+                        elif [ "$option" == "2" ]
+                        then
+                                echo "**********************************"
+                                ans=$((first-second))
+                                echo "$first subtract $second --> $ans"
+                        elif [ "$option" == "3" ]
+                        then
+                                echo "**********************************"
+                                ans=$((first*second))
+                                echo "$first multiply $second --> $ans"
+                        elif [ "$option" == "4" ]
+                        then
+                                echo "**********************************"
+                                ans=$((first**second))
+                                echo "$first power $second --> $ans"
+                        else
+                                echo "**********************************"
+                                echo "Invalid user input"
+                        fi
+                elif [[ ! "$first" =~ ^[0-9]+$ ]] || [[ ! "$second" =~ ^[0-9]+$ ]]
+                then
+                        if [[ "$first" =~ ^[0-9]+$ ]] && [[ ! "$second" =~ ^[0-9]+$ ]]
+                        then
+                                echo "***************************************"
+                                echo "Second number field can only contain numeric digits"
+                        elif [[ ! "$first" =~ ^[0-9]+$ ]] && [[ "$second" =~ ^[0-9]+$ ]]
+                        then
+                                echo "***************************************"
+                                echo "First number field can only contain numeric digits"
+                        elif [[ ! "$first" =~ ^[0-9]+$ ]] && [[ ! "$second" =~ ^[0-9]+$ ]]
+                        then
+                                echo "***************************************"
+                                echo "First number field can only contain numeric digits"
+                                echo "Second number field can only contain numeric digits"
+                        fi
+                fi
+        fi
+}
+
+
+
+user_input
+validation_1
+validation_2
+
