1af636701acee41a6e8008ea60aeb1542aa6b2cf
Hrishi Ghosalkar <87890571+rookie-cookie-8@users.noreply.github.com>
2024-08-07 09:30:31 +0530

    Update additon_odd_even.sh

--- additon_odd_even.sh
+++ additon_odd_even.sh
@@ -115,3 +115,89 @@ then
         fi
 fi
 
+------------------------------------------------------------------------------------------------------------------------------------------------
+#advanced with functions
+
+#!/bin/bash
+
+echo "Enter the first number"
+read first
+echo "Enter the second number"
+read second
+
+function first_number {
+        [[ "$first" =~ ^[0-9]+$ ]]
+}
+function second_number {
+        [[ "$second" =~ ^[0-9]+$ ]]
+}
+
+function first_number_false {
+        [[ ! "$first" =~ ^[0-9]+$ ]]
+}
+function second_number_false {
+        [[ ! "$second" =~ ^[0-9]+$ ]]
+}
+
+if [ -z "$first" ] || [ -z "$second" ]
+then
+        if [ -n "$first" ] && [ -z "$second" ]
+        then
+                echo "**********************************"
+                echo "Second number field is empty"
+        elif [ -z "$first" ] && [ -n "$second" ]
+        then
+                echo "**********************************"
+                echo "First number field is empty"
+        elif [ -z "$first" ] && [ -z "$second" ]
+        then
+                echo "**********************************"
+                echo "First number field is empty"
+                echo "Second number field is empty"
+        else
+                echo "**********************************"
+                echo "Issues with the user input"
+        fi
+elif [ -n "$first" ] && [ -n "$second" ]
+then
+        if first_number && second_number
+        then
+                ans=$((first+second))
+                if (($ans%2==0))
+                then
+                        echo "*************************************"
+                        echo "$first plus $second --> $ans"
+                        echo "$ans --> even number"
+                elif (($ans%2!=0))
+                then
+                        echo "*************************************"
+                        echo "$first plus $second --> $ans"
+                        echo "$ans --> odd number"
+                else
+                        echo "*************************************"
+                        echo "Issues with the user input"
+                fi
+        elif first_number_false || second_number_false
+        then
+                if first_number && second_number_false
+                then
+                        echo "************************************"
+                        echo "Second number field must contain only digits"
+                elif first_number_false && second_number
+                then
+                        echo "************************************"
+                        echo "First number field must contain only digits"
+                elif first_number_false && second_number_false
+                then
+                        echo "************************************"
+                        echo "First number field must contain only digits"
+                        echo "Second number field must contain only digits"
+                else
+                        echo "************************************"
+                        echo "Issues with checking the user input"
+
+                fi
+        fi
+fi
+
+
