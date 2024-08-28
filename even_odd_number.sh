3f932230b9bfb5a79a27f885decd65f662816a7e
Hrishi Ghosalkar <87890571+rookie-cookie-8@users.noreply.github.com>
2024-08-28 16:20:36 +0530

    Update even_odd_number.sh

--- even_odd_number.sh
+++ even_odd_number.sh
@@ -73,94 +73,104 @@ read first
 echo "Enter the second number"
 read second
 
+function first_check {
+        [[ $first =~ ^[0-9]+$ ]]
+}
+function second_check {
+        [[ $second =~ ^[0-9]+$ ]]
+}
+function first_wrong {
+        [[ ! $first =~ ^[0-9]+$ ]]
+}
+function second_wrong {
+        [[ ! $second =~ ^[0-9]+$ ]]
+}
 
 if [ -z "$first" ] || [ -z "$second" ]
 then
         if [ -n "$first" ] && [ -z "$second" ]
         then
-                echo "************************************"
+                echo "********************************"
                 echo "Second number field is empty"
         elif [ -z "$first" ] && [ -n "$second" ]
         then
-                echo "************************************"
+                echo "********************************"
                 echo "First number field is empty"
         elif [ -z "$first" ] && [ -z "$second" ]
         then
-                echo "************************************"
+                echo "********************************"
                 echo "First number field is empty"
                 echo "Second number field is empty"
         else
-                echo "************************************"
+                echo "********************************"
                 echo "Issues with the user input"
         fi
 elif [ -n "$first" ] && [ -n "$second" ]
 then
-        if [[ "$first" =~ ^[0-9]+$ ]] && [[ "$second" =~ ^[0-9]+$ ]]
+        if first_wrong || second_wrong
+        then
+                if first_check && second_wrong
+                then
+                        echo "******************************"
+                        echo "Second number field can only contain numeric digits"
+                elif first_wrong && second_check
+                then
+                        echo "******************************"
+                        echo "First number field can only contain numeric digits"
+                elif first_wrong && second_wrong
+                then
+                        echo "******************************"
+                        echo "First number field can only contain numeric digits"
+                        echo "Second number field can only contain numeric digits"
+                else
+                        echo "******************************"
+                        echo "Some issues while check user input"
+                fi
+        elif first_check && second_check
         then
                 if [ $first -lt $second ]
                 then
-                        echo "**********************************************"
+                        echo "****************************************"
                         echo "Would you like to print even or odd numbers"
                         read option
                         if [ "$option" == "even" ]
                         then
-                                for even_numbers in $(seq $first $second)
+                                for even_number in $(seq $first $second)
                                 do
-                                        if (($even_numbers%2==0))
+                                        if (($even_number%2==0))
                                         then
-                                                echo "Number is --> $even_numbers"
-                                                sleep 0.2
+                                                echo "Even number is --> $even_number"
+                                                sleep 0.5
                                         else
                                                 continue
                                         fi
                                 done
                         elif [ "$option" == "odd" ]
                         then
-                                for odd_numbers in $(seq $first $second)
+                                for odd_number in $(seq $first $second)
                                 do
-                                        if (($odd_numbers%2!=0))
+                                        if (($odd_number%2!=0))
                                         then
-                                                echo "Number is --> $odd_numbers"
-                                                sleep 0.2
+                                                echo "Even number is --> $odd_number"
+                                                sleep 0.5
                                         else
                                                 continue
                                         fi
                                 done
                         else
-                                echo "*****************************************"
+                                echo "**************************************"
                                 echo "Invalid user input"
                         fi
-                elif [ $first -gt $second ] || [ $first -eq $second ]
+                elif [ $first -eq $second ]
                 then
-                        if [ $first -eq $second ]
-                        then
-                                echo "********************************"
-                                echo "$first is equal to $second"
-                                echo "Cannot do anything"
-                        elif [ $first -gt $second ]
-                        then
-                                echo "********************************"
-                                echo "$first is greater than $second"
-                                echo "Cannot do anything"
-                        fi
-
-                fi
-        elif [[ ! "$first" =~ ^[0-9]+$ ]] || [[ ! "$second" =~ ^[0-9]+$ ]]
-        then
-                if [[ ! "$first" =~ ^[0-9]+$ ]] && [[ "$second" =~ ^[0-9]+$ ]]
+                        echo "*****************************"
+                        echo "First number is equal to second number"
+                        echo "Cannot do anything"
+                elif [ $first -gt $second ]
                 then
-                        echo "*************************************"
-                        echo "First number field must contain only numeric digits"
-                elif [[ "$first" =~ ^[0-9]+$ ]] && [[ ! "$second" =~ ^[0-9]+$ ]]
-                then
-                        echo "*************************************"
-                        echo "Second number field must contain only numeric digits"
-                elif [[ ! "$first" =~ ^[0-9]+$ ]] && [[ ! "$second" =~ ^[0-9]+$ ]]
-                then
-                        echo "*************************************"
-                        echo "First number field must contain only numeric digits"
-                        echo "Second number field must contain only numeric digits"
+                        echo "*****************************"
+                        echo "First number is greater than second number"
+                        echo "Cannot do anything"
                 fi
         fi
 fi
-
