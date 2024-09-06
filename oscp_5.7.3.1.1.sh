a2ff00dc0b330a0c92fe7fe7d3434795e81c2c65
Hrishi Ghosalkar <87890571+rookie-cookie-8@users.noreply.github.com>
2024-09-06 15:55:24 +0530

    Update oscp_5.7.3.1.1.sh

--- oscp_5.7.3.1.1.sh
+++ oscp_5.7.3.1.1.sh
@@ -82,30 +82,31 @@ rm -rf ./network-sweep
 #advanced
 #!/bin/bash
 
-echo "Enter the network part"
+
+echo "Enter the network part i.e. 1.1.1"
 read network
 echo "Enter the first host"
 read first
 echo "Enter the last host"
 read last
 
-function network_check {
-        [[ "$network" =~ ^[0-9.]+$ ]]
+function network_right {
+        [[ $network =~ ^[0-9.]+$ ]]
 }
 function network_wrong {
-        [[ ! "$network" =~ ^[0-9.]+$ ]]
+        [[ ! $network =~ ^[0-9.]+$ ]]
 }
-function first_check {
-        [[ "$first" =~ ^[0-9]+$ ]]
+function first_right {
+        [[ $first =~ ^[0-9]+$ ]]
 }
 function first_wrong {
-        [[ ! "$first" =~ ^[0-9]+$ ]]
+        [[ ! $first =~ ^[0-9]+$ ]]
 }
-function last_check {
-        [[ "$last" =~ ^.?[0-9]+$ ]]
+function last_right {
+        [[ $last =~ ^[0-9]+$ ]]
 }
 function last_wrong {
-        [[ ! "$last" =~ ^[0-9]+$ ]]
+        [[ ! $last =~ ^[0-9]+$ ]]
 }
 
 
@@ -124,15 +125,15 @@ then
         then
                 echo "**************************************"
                 echo "First host field is empty"
-        elif [ -z "$network" ] && [ -n "$first" ] && [ -n "$last" ]
-        then
-                echo "**************************************"
-                echo "Network field is empty"
         elif [ -z "$network" ] && [ -z "$first" ] && [ -n "$last" ]
         then
                 echo "**************************************"
                 echo "Network field is empty"
                 echo "First host field is empty"
+        elif [ -z "$network" ] && [ -n "$first" ] && [ -n "$last" ]
+        then
+                echo "**************************************"
+                echo "Network field is empty"
         elif [ -z "$network" ] && [ -n "$first" ] && [ -z "$last" ]
         then
                 echo "**************************************"
@@ -144,66 +145,63 @@ then
                 echo "Network field is empty"
                 echo "First host field is empty"
                 echo "Last host field is empty"
-        else
-                echo "**************************************"
-                echo "Issues with the user input"
         fi
 elif [ -n "$network" ] && [ -n "$first" ] && [ -n "$last" ]
 then
         if network_wrong || first_wrong || last_wrong
         then
-                if network_check && first_wrong && last_wrong
+                if network_right && first_wrong && last_wrong
                 then
-                        echo "***********************************"
-                        echo "First host field can only contain numeric digits"
-                        echo "Last host field can only contain numeric digits"
-                elif network_check && first_check && last_wrong
+                        echo "*****************************************"
+                        echo "First host field must contain numeric digits only"
+                        echo "Last host field must contain numeric digits only"
+                elif network_right && first_right && last_wrong
                 then
-                        echo "***********************************"
-                        echo "Last host field can only contain numeric digits"
-                elif network_check && first_wrong && last_check
+                        echo "*****************************************"
+                        echo "Last host field must contain numeric digits only"
+                elif network_right && first_wrong && last_right
                 then
-                        echo "***********************************"
-                        echo "First host field can only contain numeric digits"
-                elif network_wrong && first_check && last_check
+                        echo "*****************************************"
+                        echo "First host field must contain numeric digits only"
+                elif network_wrong && first_wrong && last_right
                 then
-                        echo "***********************************"
-                        echo "Network field can only contain numeric digits"
-                elif network_wrong && first_wrong && last_check
+                        echo "*****************************************"
+                        echo "Network field must contain numeric digits only"
+                        echo "First host field must contain numeric digits only"
+                elif network_wrong && first_right && last_right
                 then
-                        echo "***********************************"
-                        echo "Network field can only contain numeric digits"
-                        echo "First host field can only contain numeric digits"
-                elif network_wrong && first_check && last_wrong
+                        echo "*****************************************"
+                        echo "Network field must contain numeric digits only"
+                        echo "First host field must contain numeric digits only"
+                elif network_wrong && first_right && last_wrong
                 then
-                        echo "***********************************"
-                        echo "Network field can only contain numeric digits"
-                        echo "Last host field can only contain numeric digits"
+                        echo "*****************************************"
+                        echo "Network field must contain numeric digits only"
+                        echo "First host field must contain numeric digits only"
                 elif network_wrong && first_wrong && last_wrong
                 then
-                        echo "***********************************"
-                        echo "Network field can only contain numeric digits"
-                        echo "First host field can only contain numeric digits"
-                        echo "Last host field can only contain numeric digits"
+                        echo "*****************************************"
+                        echo "Network field must contain numeric digits only"
+                        echo "First host field must contain numeric digits only"
+                        echo "Last host field must contain numeric digits only"
                 fi
-        elif network_check && first_check && last_check
+        elif network_right && first_right && last_right
         then
-                for host_alive in $(seq $first $last)
+                echo "******Checking $network.$first-$last**********"
+                for ip in $(seq $first $last)
                 do
-                        echo "*******Checking $network.$host_alive**************"
-                        ping -c 1 $network.$host_alive | grep -i "64 bytes" | cut -d " " -f4 | tr -d ":" 1>> ./host_alive_lists
-                        sleep 0.5
+                        ping -c 1 $network.$ip | grep -i "64 bytes" | cut -d " " -f4 | tr -d ":" 2>/dev/null 1>>host_alive &
+                        sleep 0.2
                 done
-                if [ -s ./host_alive_lists ]
+                if [ -s ./host_alive ]
                 then
-                        echo "************************************"
-                        cat ./host_alive_lists
+                        echo "******Host alive ******"
+                        cat host_alive
                 else
-                        echo "************************************"
-                        echo "No active host found"
+                        echo "****NO host active******"
                 fi
+
         fi
 fi
 rm -rf ./1
-rm -rf ./host_alive_lists
-
+rm -rf ./host_alive
