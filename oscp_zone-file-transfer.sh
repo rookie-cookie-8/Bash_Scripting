1a792701b0f4956c0d0edc44c3f9cf73a680e3a1
Hrishi Ghosalkar <87890571+rookie-cookie-8@users.noreply.github.com>
2024-09-12 12:26:41 +0530

    Update oscp_zone-file-transfer.sh

--- oscp_zone-file-transfer.sh
+++ oscp_zone-file-transfer.sh
@@ -27,3 +27,56 @@ else
         fi
 
 fi
+------------------------------------------------------------------------------------------------------------------------------------------------------------
+#!/bin/bash
+
+
+echo "Enter the domain name"
+read domain
+
+if [ -z "$domain" ]
+then
+        echo "*******************************"
+        echo "Domain field is empty"
+else
+        echo "*******Looking for DNS servers********"
+        sleep 0.3
+        host -t ns $domain &>/dev/null
+        if [ $? -eq 0 ]
+        then
+                echo "*******Name Servers found*********"
+                sleep 0.3
+                host -t ns $domain | cut -d " " -f4 > ./name-servers
+                sleep 0.3
+                echo "Would you like to do a zone file transfer y/n"
+                read choice
+                if [ "$choice" == "y" ]
+                then
+                        for zone_transfer in $(cat ./name-servers)
+                        do
+                                host -l $domain $zone_transfer &>/dev/null
+                                if [ $? -eq 0 ]
+                                then
+                                        echo "*******$zone_transfer Done********"
+                                        host -l $domain $zone_transfer
+
+                                else
+                                        echo "*******$zone_transfer failed********"
+                                        sleep 0.5
+                                        continue
+                                fi
+                        done
+                elif [ "$choice" == "n" ]
+                then
+                        echo "*******No zone file tarnsfer conducted as per user request********"
+                else
+                        echo "********Invalid user input***********"
+                fi
+        else
+                echo "**********************************"
+                echo "Name servers not found"
+        fi
+fi
+rm -rf ./1
+rm -rf ./name-servers
+
