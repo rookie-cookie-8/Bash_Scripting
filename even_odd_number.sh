8ce138dc43c25e3a5a62d3fe329fd9c6333ecf28
Hrishi Ghosalkar <87890571+rookie-cookie-8@users.noreply.github.com>
2024-09-23 16:15:44 +0530

    Update even_odd_number.sh

--- even_odd_number.sh
+++ even_odd_number.sh
@@ -174,3 +174,145 @@ then
                 fi
         fi
 fi
+------------------------------------------------------------------------------------------------------------------------------------------------------------
+#!/bin/bash
+
+echo "Enter the first number"
+read first
+echo "Enter the second number"
+read second
+
+function first_right {
+        [[ $first =~ [0-9]+$ ]]
+}
+function first_wrong {
+        [[ ! $first =~ [0-9]+$ ]]
+}
+function second_right {
+        [[ $second =~ [0-9]+$ ]]
+}
+function second_wrong {
+        [[ ! $second =~ [0-9]+$ ]]
+}
+
+
+if [ -z "$first" ] || [ -z "$second" ]
+then
+        if [ -n "$first" ] && [ -z "$second" ]
+        then
+                echo "**********************************"
+                echo "Second number field is empty"
+        elif [ -z "$first" ] && [ -n "$second" ]
+        then
+                echo "**********************************"
+                echo "First number field is empty"
+        elif [ -z "$first" ] && [ -z "$second" ]
+        then
+                echo "**********************************"
+                echo "First number field is empty"
+                echo "Second number field is empty"
+        else
+                echo "**********************************"
+                echo "Issues with the user input"
+        fi
+elif [ -n "$first" ] && [ -n "$second" ]
+then
+        if first_wrong || second_wrong
+        then
+                if first_right && second_wrong
+                then
+                        echo "***************************************"
+                        echo "Second number field must contain only digits"
+                elif first_wrong && second_right
+                then
+                        echo "***************************************"
+                        echo "First number field must contain only digits"
+                elif first_wrong && second_wrong
+                then
+                        echo "***************************************"
+                        echo "First number field must contain only digits"
+                        echo "Second number field must contain only digits"
+                else
+                        echo "***************************************"
+                        echo "Issues with user input validation"
+                fi
+        elif first_right && second_right
+        then
+                if [ $first -lt $second ]
+                then
+                        echo "*****************************************"
+                        echo "Would you like to print even or odd numbers"
+                        read option
+                        if [ "$option" == "even" ]
+                        then
+                                for even_numbers in $(seq $first $second)
+                                do
+                                        if (($even_numbers%2==0))
+                                        then
+                                                echo "Number is $even_numbers"
+                                                sleep 0.5
+                                        else
+                                                continue
+                                        fi
+
+                                done
+                        elif [ "$option" == "odd" ]
+                        then
+                                for odd_numbers in $(seq $first $second)
+                                do
+                                        if (($odd_numbers%2!=0))
+                                        then
+                                                echo "Number is $odd_numbers"
+                                                sleep 0.5
+                                        else
+                                                continue
+                                        fi
+                                done
+
+                        else
+                                echo "**************************************"
+                                echo "Invalid user option"
+                        fi
+                elif [ $first -gt $second ]
+                then
+                        echo "***********************************"
+                        echo "Would you like to print even or odd numbers"
+                        read option
+                        if [ "$option" == "even" ]
+                        then
+                                for even_numbers in $(seq $second $first)
+                                do
+                                        if (($even_numbers%2==0))
+                                        then
+                                                echo "Number is $even_numbers"
+                                                sleep 0.5
+                                        else
+                                                continue
+                                        fi
+
+                                done
+                        elif [ "$option" == "odd" ]
+                        then
+                                for odd_numbers in $(seq $second $first)
+                                do
+                                        if (($odd_numbers%2!=0))
+                                        then
+                                                echo "Number is $odd_numbers"
+                                                sleep 0.5
+                                        else
+                                                continue
+                                        fi
+                                done
+                        fi
+
+                elif [ $first -eq $second ]
+                then
+                        echo "***********************************"
+                        echo "First number cannot be equal to second"
+                else
+                        echo "***********************************"
+                        echo "Issues with checking numbers"
+                fi
+        fi
+fi
+
