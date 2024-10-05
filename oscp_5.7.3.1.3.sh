dbe086d94c5058c7676e904f29b22a5afca84f37
Hrishi Ghosalkar <87890571+rookie-cookie-8@users.noreply.github.com>
2024-10-05 11:40:43 +0530

    Update oscp_5.7.3.1.3.sh

--- oscp_5.7.3.1.3.sh
+++ oscp_5.7.3.1.3.sh
@@ -45,39 +45,37 @@ rm -rf ./javascript-files
 -----------------------------------------------------------------------------------------------------------------------------------------------------------------------
 #!/bin/bash
 
-echo "Enter the full file path"
+echo "Enter the filepath"
 read filepath
 
 if [ -z "$filepath" ]
 then
-        echo "*********************************"
+        echo "*********************************************"
         echo "File path field is empty"
 else
-        if [ -e $filepath ]
+        if [ -f $filepath ]
         then
-                if [ -f $filepath ]
+                echo "********Looking for JavaScript files*******"
+                sleep 0.5
+                cat $filepath | grep -i "js" | cut -d "/" -f5 | cut -d " " -f1 | sort |uniq > ./javascript_files
+                if [ -s ./javascript_files ]
                 then
-                        cat $filepath | grep -i "js" | cut -d "/" -f5 | cut -d " " -f1 | sort | uniq 1> javascript_files
-                        if [ -s ./javascript_files ]
-                        then
-                                echo "******Javascript file found********"
-                                cat javascript_files
-                        else
-                                echo "******No Javascript file found********"
-                        fi
-                elif [ -d $filepath ]
-                then
-                        echo "***********************************"
-                        echo "Its a directory"
+                        cat ./javascript_files
                 else
-                        echo "Invalid file type"
+                        echo "*******No javascript files found**********"
                 fi
+        elif [ -d $filepath ]
+        then
+                echo "*************************************"
+                echo "its a directory, nothing can be done"
+        elif [ -e $filepath ]
+        then
+                echo "*************************************"
+                echo "Unable to identify the file type"
         else
-                echo "********************************"
+                echo "*************************************"
                 echo "File not found"
         fi
-
 fi
-rm -rf ./1
 rm -rf ./javascript_files
-
+rm -rf ./1
