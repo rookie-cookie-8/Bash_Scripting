bda0059d71187d19178f018bdee266491c5ece67
Hrishi Ghosalkar <87890571+rookie-cookie-8@users.noreply.github.com>
2024-10-05 11:04:56 +0530

    Update oscp_5.7.3.1.1.sh

--- oscp_5.7.3.1.1.sh
+++ oscp_5.7.3.1.1.sh
@@ -82,7 +82,6 @@ rm -rf ./network-sweep
 #advanced
 #!/bin/bash
 
-
 echo "Enter the network part i.e. 1.1.1"
 read network
 echo "Enter the first host"
@@ -108,43 +107,44 @@ function last_right {
 function last_wrong {
         [[ ! $last =~ ^[0-9]+$ ]]
 }
-
-
 if [ -z "$network" ] || [ -z "$first" ] || [ -z "$last" ]
 then
         if [ -n "$network" ] && [ -z "$first" ] && [ -z "$last" ]
         then
-                echo "**************************************"
+                echo "***************************************************"
                 echo "First host field is empty"
                 echo "Last host field is empty"
         elif [ -n "$network" ] && [ -n "$first" ] && [ -z "$last" ]
         then
-                echo "**************************************"
+                echo "***************************************************"
                 echo "Last host field is empty"
         elif [ -n "$network" ] && [ -z "$first" ] && [ -n "$last" ]
         then
-                echo "**************************************"
+                echo "***************************************************"
                 echo "First host field is empty"
         elif [ -z "$network" ] && [ -z "$first" ] && [ -n "$last" ]
         then
-                echo "**************************************"
+                echo "***************************************************"
                 echo "Network field is empty"
                 echo "First host field is empty"
         elif [ -z "$network" ] && [ -n "$first" ] && [ -n "$last" ]
         then
-                echo "**************************************"
+                echo "***************************************************"
                 echo "Network field is empty"
         elif [ -z "$network" ] && [ -n "$first" ] && [ -z "$last" ]
         then
-                echo "**************************************"
+                echo "***************************************************"
                 echo "Network field is empty"
                 echo "Last host field is empty"
         elif [ -z "$network" ] && [ -z "$first" ] && [ -z "$last" ]
         then
-                echo "**************************************"
+                echo "***************************************************"
                 echo "Network field is empty"
                 echo "First host field is empty"
                 echo "Last host field is empty"
+        else
+                echo "***************************************************"
+                echo "Issues with the user input"
         fi
 elif [ -n "$network" ] && [ -n "$first" ] && [ -n "$last" ]
 then
@@ -152,56 +152,57 @@ then
         then
                 if network_right && first_wrong && last_wrong
                 then
-                        echo "*****************************************"
-                        echo "First host field must contain numeric digits only"
-                        echo "Last host field must contain numeric digits only"
+                        echo "*************************************************"
+                        echo "First host field must contain conly numeric values"
+                        echo "Last host field must contain conly numeric values"
                 elif network_right && first_right && last_wrong
                 then
-                        echo "*****************************************"
-                        echo "Last host field must contain numeric digits only"
+                        echo "*************************************************"
+                        echo "First host field must contain conly numeric values"
                 elif network_right && first_wrong && last_right
                 then
-                        echo "*****************************************"
-                        echo "First host field must contain numeric digits only"
+                        echo "*************************************************"
+                        echo "Last host field must contain conly numeric values"
                 elif network_wrong && first_wrong && last_right
                 then
-                        echo "*****************************************"
-                        echo "Network field must contain numeric digits only"
-                        echo "First host field must contain numeric digits only"
+                        echo "*************************************************"
+                        echo "Network field must contain only numeric and special character '.'"
+                        echo "First host field must contain conly numeric values"
                 elif network_wrong && first_right && last_right
                 then
-                        echo "*****************************************"
-                        echo "Network field must contain numeric digits only"
-                        echo "First host field must contain numeric digits only"
+                        echo "*************************************************"
+                        echo "Network field must contain only numeric and special character '.'"
                 elif network_wrong && first_right && last_wrong
                 then
-                        echo "*****************************************"
-                        echo "Network field must contain numeric digits only"
-                        echo "First host field must contain numeric digits only"
+                        echo "*************************************************"
+                        echo "Network field must contain only numeric and special character '.'"
+                        echo "Last host field must contain conly numeric values"
                 elif network_wrong && first_wrong && last_wrong
                 then
-                        echo "*****************************************"
-                        echo "Network field must contain numeric digits only"
-                        echo "First host field must contain numeric digits only"
-                        echo "Last host field must contain numeric digits only"
+                        echo "*************************************************"
+                        echo "Network field must contain only numeric and special character '.'"
+                        echo "First host field must contain conly numeric values"
+                        echo "Last host field must contain conly numeric values"
+                else
+                        echo "*************************************************"
+                        echo "Issues with the user input validation"
                 fi
         elif network_right && first_right && last_right
         then
-                echo "******Checking $network.$first-$last**********"
-                for ip in $(seq $first $last)
+                echo "***********Checking active hosts*********************"
+                for host_alive in $(seq $first $last)
                 do
-                        ping -c 1 $network.$ip | grep -i "64 bytes" | cut -d " " -f4 | tr -d ":" 2>/dev/null 1>>host_alive &
-                        sleep 0.2
+                        ping -c 1 $network.$host_alive | grep -i "64 bytes" | cut -d " " -f4 | tr -d ":" >> ./host_alive_lists &
+                        sleep 0.5
                 done
-                if [ -s ./host_alive ]
+                if [ -s ./host_alive_lists ]
                 then
-                        echo "******Host alive ******"
-                        cat host_alive
+                        echo "*****Active hosts lists****************"
+                        cat ./host_alive_lists
                 else
-                        echo "****NO host active******"
+                        echo "*********No host found***************"
                 fi
-
         fi
 fi
+rm -rf ./host_alive_lists
 rm -rf ./1
-rm -rf ./host_alive
