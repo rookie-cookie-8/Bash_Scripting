6d982a6a3d3e7c1b751ad22974dce38c511848c4
Hrishi Ghosalkar <87890571+rookie-cookie-8@users.noreply.github.com>
2024-10-15 13:15:15 +0530

    Update caculation_while_functions.sh

--- caculation_while_functions.sh
+++ caculation_while_functions.sh
@@ -247,3 +247,187 @@ function try_again {
 user_input
 validation_1
 validation_2
+
+--------------------------------------------------------------------------------------------------------------------------------------------------------------------
+#calculation such as add, subtract, multiply and power
+#user input must not be greather than 3 positive numbers 
+
+#!/bin/bash
+
+function user_input {
+        echo "Enter the first number"
+        read first
+        echo "Enter the second number"
+        read second
+}
+
+function first_right {
+        [[ $first =~ ^[0-9]+$ ]]
+}
+function second_right {
+        [[ $second =~ ^[0-9]+$ ]]
+}
+function first_wrong {
+        [[ ! $first =~ ^[0-9]+$ ]]
+}
+function second_wrong {
+        [[ ! $second =~ ^[0-9]+$ ]]
+}
+function first_right_range {
+        [[ $first =~ ^[0-9]{1,3}$ ]]
+}
+function second_right_range {
+        [[ $second =~ ^[0-9]{1,3}$ ]]
+}
+function first_wrong_range {
+        [[ ! $first =~ ^[0-9]{1,3}$ ]]
+}
+function second_wrong_range {
+        [[ ! $second =~ ^[0-9]{1,3}$ ]]
+}
+
+function validation_1 {
+
+        if [ -z "$first" ] || [ -z "$second" ]
+        then
+                if [ -n "$first" ] && [ -z "$second" ]
+                then
+                        echo "*************************************"
+                        echo "Second number field is empty"
+                        try_again
+                elif [ -z "$first" ] && [ -n "$second" ]
+                then
+                        echo "*************************************"
+                        echo "First number field is empty"
+                        try_again
+                elif [ -z "$first" ] && [ -z "$second" ]
+                then
+                        echo "*************************************"
+                        echo "First number field is empty"
+                        echo "Second number field is empty"
+                        try_again
+                else
+                        echo "*************************************"
+                        echo "Issues with the user input"
+                        try_again
+                fi
+        fi
+}
+
+function validation_2 {
+
+        if [ -n "$first" ] && [ -n "$second" ]
+        then
+                if first_wrong || second_wrong
+                then
+                        if first_right && second_wrong
+                        then
+                                echo "**************************************"
+                                echo "Second field must contain only digits"
+                                try_again
+                        elif first_wrong && second_right
+                        then
+                                echo "**************************************"
+                                echo "First field must contain only digits"
+                                try_again
+                        elif first_wrong && second_wrong
+                        then
+                                echo "**************************************"
+                                echo "First field must contain only digits"
+                                echo "Second field must contain only digits"
+                                try_again
+                        else
+                                echo "**************************************"
+                                echo "Issues with the user input validation"
+                                try_again
+                        fi
+                elif first_right && second_right
+                then
+                        if first_wrong_range || second_wrong_range
+                        then
+                                if first_right_range && second_wrong_range
+                                then
+                                        echo "****************************************"
+                                        echo "Second number field must contain max 3 digits"
+                                        try_again
+                                elif first_wrong_range && second_right_range
+                                then
+                                        echo "****************************************"
+                                        echo "First number field must contain max 3 digits"
+                                        try_again
+                                elif first_wrong_range && second_wrong_range
+                                then
+                                        echo "****************************************"
+                                        echo "First number field must contain max 3 digits"
+                                        echo "Second number field must contain max 3 digits"
+                                        try_again
+                                else
+                                        echo "****************************************"
+                                        echo "Issues with user input numbers validation"
+                                        try_again
+                                fi
+                        elif first_right_range && second_right_range
+                        then
+                                calculation
+                        fi
+                fi
+        fi
+}
+
+function calculation {
+        echo "**********************************************"
+        echo "Press 1 --> Addition"
+        echo "Press 3 --> Subtraction"
+        echo "Press 3 --> Multiplication"
+        echo "Press 4 --> Power"
+        read choice
+        if [ $choice == "1" ]
+        then
+                ans=$(($first+$second))
+                echo "$first plus $second --> $ans"
+                try_again
+        elif [ $choice == "2" ]
+        then
+                ans=$(($first-$second))
+                echo "$first minus $second --> $ans"
+                try_again
+        elif [ $choice == "3" ]
+        then
+                ans=$(($first*$second))
+                echo "$first multiply $second --> $ans"
+                try_again
+        elif [ $choice == "4" ]
+        then
+                ans=$(($first**$second))
+                echo "$first power $second --> $ans"
+                try_again
+        else
+                echo "Invalid user input"
+                try_again
+        fi
+}
+
+
+function try_again {
+        echo "*****************************************"
+        echo "Would you like to try again y/n"
+        read answer
+        if [ "$answer" == "y" ]
+        then
+                user_input
+                validation_1
+                validation_2
+        elif [ "$answer" == "n" ]
+        then
+                echo "*****************************************"
+                echo "You choose to quit, bye"
+        else
+                echo "*****************************************"
+                echo "Invalid user input, bye"
+        fi
+}
+
+user_input
+validation_1
+validation_2
+
