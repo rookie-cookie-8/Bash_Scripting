507a1482c91f71390c63a8878f51b46759d9c815
Hrishi Ghosalkar <87890571+rookie-cookie-8@users.noreply.github.com>
2024-10-15 10:50:12 +0530

    Update additon_odd_even.sh

--- additon_odd_even.sh
+++ additon_odd_even.sh
@@ -1,5 +1,5 @@
-#two numbers as input from users
-#add the two numbers and whether the result of addition is even or odd number 
+#accept two numbers from the user input
+#add the two numbers, the script will check whether the result i.e. addition is odd or even number
 
 #!/bin/bash
 
@@ -50,7 +50,9 @@ then
 fi
 
 ------------------------------------------------------------------------------------------------------------------------------------------------
-#advanced 
+#accept two numbers from the user input
+#add the two numbers, the script will check whether the result i.e. addition is odd or even number
+#user input checking --> Must be numeric digits no alphabets
 
 #!/bin/bash
 
@@ -116,8 +118,10 @@ then
 fi
 
 ------------------------------------------------------------------------------------------------------------------------------------------------
-#advanced with functions
-#user input can be in negative
+#accept two numbers from the user input
+#add the two numbers, the script will check whether the result i.e. addition is odd or even number
+#user input checking --> Must be numeric digits no alphabets
+#with functions and negative numbers as user input accepted
 
 #!/bin/bash
 
@@ -202,3 +206,130 @@ fi
 
 
 --------------------------------------------------------------------------------------------------------------------------------------------------
+#accept two numbers from the user input
+#add the two numbers, the script will check whether the result i.e. addition is odd or even number
+#user input checking --> Must be numeric digits no alphabets
+#with functions and negative numbers as user input accepted
+#user input must not be greater than 3 digits
+
+#!/bin/bash
+
+echo "Enter the first number"
+read first
+echo "Enter the second number"
+read second
+
+function first_right {
+        [[ $first =~ ^[0-9-]+$ ]]
+}
+function first_wrong {
+        [[ ! $first =~ ^[0-9-]+$ ]]
+}
+function first_range_right {
+        [[ $first =~ ^[0-9-]{1,4}$ ]]
+}
+function first_range_wrong {
+        [[ ! $first =~ ^[0-9-]{1,4}$ ]]
+}
+function second_right {
+        [[ $second =~ ^[0-9-]+$ ]]
+}
+function second_wrong {
+        [[ ! $second =~ ^[0-9-]+$ ]]
+}
+function second_range_right {
+        [[ $second =~ ^[0-9-]{1,4}$ ]]
+}
+function second_range_wrong {
+        [[ ! $second =~ ^[0-9-]{1,4}$ ]]
+}
+
+
+if [ -z "$first" ] || [ -z "$second" ]
+then
+        if [ -n "$first" ] && [ -z "$second" ]
+        then
+                echo "****************************************"
+                echo "Second number field is empty"
+        elif [ -z "$first" ] && [ -n "$second" ]
+        then
+                echo "****************************************"
+                echo "First number field is empty"
+        elif [ -z "$first" ] && [ -z "$second" ]
+        then
+                echo "****************************************"
+                echo "First number field is empty"
+                echo "Second number field is empty"
+        else
+                echo "****************************************"
+                echo "Issues with the user input"
+        fi
+elif [ -n "$first" ] && [ -n "$second" ]
+then
+        if first_wrong || second_wrong
+        then
+                if first_right && second_wrong
+                then
+                        echo "****************************************"
+                        echo "Second field must contain only digits"
+                elif first_wrong && second_right
+                then
+                        echo "****************************************"
+                        echo "First field must contain only digits"
+                elif first_wrong && second_wrong
+                then
+                        echo "****************************************"
+                        echo "First field must contain only digits"
+                        echo "Second field must contain only digits"
+                else
+                        echo "****************************************"
+                        echo "Issues with the user input validation"
+                fi
+        elif first_right && second_right
+        then
+                if first_range_wrong || second_range_wrong
+                then
+                        if first_range_right && second_range_wrong
+                        then
+                                echo "****************************************"
+                                echo "Second number field must contain max 3 digits"
+                        elif first_range_wrong && second_range_right
+                        then
+                                echo "****************************************"
+                                echo "First number field must contain max 3 digits"
+                        elif first_range_wrong && second_range_wrong
+                        then
+                                echo "****************************************"
+                                echo "First number field must contain max 3 digits"
+                                echo "Second number field must contain max 3 digits"
+                        else
+                                echo "****************************************"
+                                echo "Something went wrong while checking user input range"
+                        fi
+                elif first_range_right && second_range_right
+                then
+                        ans=$(($first+$second))
+                        if (($ans%2==0))
+                        then
+                                echo "**********************************************"
+                                echo "$first plus $second is --> $ans"
+                                echo "$ans is --> Even number"
+                        elif (($ans%2!=0))
+                        then
+                                echo "**********************************************"
+                                echo "$first plus $second is --> $ans"
+                                echo "$ans is --> Odd number"
+                        else
+                                echo "**********************************************"
+                                echo "Something went wrong while additon"
+                        fi
+                fi
+        fi
+fi
+
+
+
+
+
+
+
